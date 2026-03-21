4edca0c91a420a376be958ec7f35105b19e5165c
j0hn_char <jpainter1002@gmail.com>
2026-03-22 00:38:00 +0200

    Add tile types wirh colors (dirt, stone air)

--- TerrariaCL/Assets/Scripts/TileManager.cs
+++ TerrariaCL/Assets/Scripts/TileManager.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 
+public enum TileType
+{
+    Air = 0,
+    Dirt = 1,
+    Stone = 2
+}
+
 public class TileManager : MonoBehaviour
 {
     public int worldWidth = 100;
     public int worldHeight = 50;
 
-    private int[,] tiles;
+    private TileType[,] tiles;
 
     void Start()
     {
@@ -15,19 +22,23 @@ public class TileManager : MonoBehaviour
 
     void GenerateWorld()
     {
-        tiles = new int[worldWidth, worldHeight];
+        tiles = new TileType[worldWidth, worldHeight];
 
         for (int x = 0; x < worldWidth; x++)
         {
             for (int y = 0; y < worldHeight; y++)
             {
-                if (y < worldHeight / 2)
+                if (y < worldHeight / 4)
                 {
-                    tiles[x, y] = 1; // solid
+                    tiles[x, y] = TileType.Stone;
+                }
+                else if (y < worldHeight / 2)
+                {
+                    tiles[x, y] = TileType.Dirt; // solid
                 }
                 else
                 {
-                    tiles[x, y] = 0; // air
+                    tiles[x, y] = TileType.Air; // air
                 }
             }
         }
@@ -39,12 +50,33 @@ public class TileManager : MonoBehaviour
         {
             for (int y = 0; y < worldHeight; y++)
             {
-                if (tiles[x, y] == 1)
+                if (tiles[x, y] == TileType.Air)
                 {
-                    GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Quad);
-                    tile.transform.position = new Vector3(x, y, 0);
+                    continue;
                 }
+
+                GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                tile.transform.position = new Vector3(x, y, 0);
+
+                Color tileColor = GetTileColor(tiles[x, y]);
+                Renderer renderer = tile.GetComponent<Renderer>();
+                Material mat = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+                mat.color = tileColor;
+                renderer.material = mat;
             }
+        }
+    }
+
+    Color GetTileColor(TileType type)
+    {
+        switch (type)
+        {
+            case TileType.Dirt:
+                return new Color(0.6f, 0.4f, 0.2f);
+            case TileType.Stone:
+                return new Color(0.5f, 0.5f, 0.5f);
+            default:
+                return Color.white;
         }
     }
 }

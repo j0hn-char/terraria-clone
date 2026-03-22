4c458f5a020efe790ce3bdcf8eaca5232cc6587f
j0hn_char <jpainter1002@gmail.com>
2026-03-22 20:23:44 +0200

    Switch to Unity Tilemap system with real Terraria sprites

--- TerrariaCL/Assets/Scripts/TileManager.cs
+++ TerrariaCL/Assets/Scripts/TileManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public enum TileType
 {
@@ -9,12 +10,16 @@ public enum TileType
 
 public class TileManager : MonoBehaviour
 {
-    public float seed;
     public int worldWidth = 100;
     public int worldHeight = 50;
-    private TileType[,] tiles;
-    private GameObject[,] tileObjects;
     public float noiseScale = 0.1f;
+    public float seed;
+
+    public Tilemap tilemap;
+    public RuleTile dirtTile;
+    public RuleTile stoneTile;
+
+    private TileType[,] tiles;
 
     void Start()
     {
@@ -24,9 +29,8 @@ public class TileManager : MonoBehaviour
 
     void GenerateWorld()
     {
-        seed = Random.Range(0f, 1000f);
+        seed = Random.Range(0f, 10000f);
         tiles = new TileType[worldWidth, worldHeight];
-        tileObjects = new GameObject[worldWidth, worldHeight];
 
         for (int x = 0; x < worldWidth; x++)
         {
@@ -35,17 +39,11 @@ public class TileManager : MonoBehaviour
             for (int y = 0; y < worldHeight; y++)
             {
                 if (y > surfaceHeight)
-                {
                     tiles[x, y] = TileType.Air;
-                }
                 else if (y > surfaceHeight - 10)
-                {
-                    tiles[x, y] = TileType.Dirt; // solid
-                }
+                    tiles[x, y] = TileType.Dirt;
                 else
-                {
-                    tiles[x, y] = TileType.Stone; // air
-                }
+                    tiles[x, y] = TileType.Stone;
             }
         }
     }
@@ -56,34 +54,19 @@ public class TileManager : MonoBehaviour
         {
             for (int y = 0; y < worldHeight; y++)
             {
-                if (tiles[x, y] == TileType.Air)
-                {
-                    continue;
-                }
-
-                GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Quad);
-                tile.transform.position = new Vector3(x, y, 0);
-                tileObjects[x, y] = tile;
-
-                Color tileColor = GetTileColor(tiles[x, y]);
-                Renderer renderer = tile.GetComponent<Renderer>();
-                Material mat = new Material(Shader.Find("Universal Render Pipeline/Lit"));
-                mat.color = tileColor;
-                renderer.material = mat;
+                TileBase tile = GetTileAsset(tiles[x, y]);
+                tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
     }
 
-    Color GetTileColor(TileType type)
+    TileBase GetTileAsset(TileType type)
     {
         switch (type)
         {
-            case TileType.Dirt:
-                return new Color(0.6f, 0.4f, 0.2f);
-            case TileType.Stone:
-                return new Color(0.5f, 0.5f, 0.5f);
-            default:
-                return Color.white;
+            case TileType.Dirt: return dirtTile;
+            case TileType.Stone: return stoneTile;
+            default: return null;
         }
     }
 
@@ -95,38 +78,17 @@ public class TileManager : MonoBehaviour
             return;
 
         tiles[x, y] = TileType.Air;
-        UpdateTileVisual(x, y);
+        tilemap.SetTile(new Vector3Int(x, y, 0), null);
     }
 
-    void UpdateTileVisual(int x, int y)
-    {
-        if (tileObjects[x, y] != null)
-        {
-            Destroy(tileObjects[x, y]);
-            tileObjects[x, y] = null;
-        }
-        else
-        {
-            GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Quad);
-            tile.transform.position = new Vector3(x, y, 0);
-            tileObjects[x, y] = tile;
-
-            Color tileColor = GetTileColor(tiles[x, y]);
-            Renderer renderer = tile.GetComponent<Renderer>();
-            Material mat = new Material(Shader.Find("Universal Render Pipeline/Lit"));
-            mat.color = tileColor;
-            renderer.material = mat;
-        }
-    }
-
-    internal void PlaceTile(int x, int y)
+    public void PlaceTile(int x, int y)
     {
         if (x < 0 || x >= worldWidth || y < 0 || y >= worldHeight)
             return;
-        if (tiles[x, y] == TileType.Stone || tiles[x, y] == TileType.Dirt)
+        if (tiles[x, y] != TileType.Air)
             return;
 
         tiles[x, y] = TileType.Dirt;
-        UpdateTileVisual(x, y);
+        tilemap.SetTile(new Vector3Int(x, y, 0), dirtTile);
     }
 }
